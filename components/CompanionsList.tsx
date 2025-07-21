33d9f89ce366d38219c32dd1738694bfd5ac8bbc
Shathish2004 <shathishkumaran07@gmail.com>
2025-07-21 06:51:21 +0530

    Modified the companionList and solved the issue in the supabase

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -33,8 +33,8 @@ const CompanionsList = ({
           </TableRow>
         </TableHeader>
         <TableBody>
-          {companions?.map(({ id, name, subject, topic, duration }) => (
-            <TableRow key={id}>
+          {companions?.map(({ id, name, subject, topic, duration }, index) => (
+            <TableRow key={index}>
               <TableCell>
                 <Link href={`/companions/${id}`}>
                   <div className="flex items-center gap-2">
